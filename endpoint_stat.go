f783e9926a282ff3f5065bd1826b9bd5274124ef
chenyongjia <yongjia.chen@hotmail.com>
2021-06-15 10:39:01 +0800

    feature: 更新Service/Endpoint的统计查询功能

---
+++ endpoint_stat.go
@@ -0,0 +1,48 @@
+package inspect
+
+import (
+	"github.com/bytepowered/flux"
+	"github.com/bytepowered/flux/ext"
+	"github.com/bytepowered/flux/transporter/inapp"
+)
+
+const (
+	EndpointStatsServiceInterface = "net.bytepowered.flux.inspect.StatsService"
+	EndpointStatsServiceMethod    = "StatsEndpoint"
+)
+
+func init() {
+	// 注册Service
+	srv := flux.Service{
+		Kind:      "flux.service/inspect/v1",
+		Interface: EndpointStatsServiceInterface,
+		Method:    EndpointStatsServiceMethod,
+		Attributes: []flux.Attribute{
+			{Name: flux.ServiceAttrTagRpcProto, Value: flux.ProtoInApp},
+		},
+	}
+	ext.RegisterService(srv)
+	inapp.RegisterInvokeFunc(srv.ServiceID(), EndpointStatsInvokeFunc)
+}
+
+// EndpointStatsInvokeFunc 查询Endpoint元数据统计的函数实现
+func EndpointStatsInvokeFunc(_ *flux.Context, _ flux.Service) (interface{}, *flux.ServeError) {
+	apps := make(map[string]int)
+	count := 0
+	for _, ep := range ext.Endpoints() {
+		count++
+		app := ep.Random().Application
+		if c, ok := apps[app]; ok {
+			apps[app] = c + 1
+		} else {
+			apps[app] = 1
+		}
+	}
+	return map[string]interface{}{
+		"success": true,
+		"data": map[string]interface{}{
+			"count": count,
+			"apps":  apps,
+		},
+	}, nil
+}
---
+++ service_stat.go
@@ -0,0 +1,48 @@
+package inspect
+
+import (
+	"github.com/bytepowered/flux"
+	"github.com/bytepowered/flux/ext"
+	"github.com/bytepowered/flux/transporter/inapp"
+)
+
+const (
+	ServiceStatsServiceInterface = "net.bytepowered.flux.inspect.StatsService"
+	ServiceStatsServiceMethod    = "StatsService"
+)
+
+func init() {
+	// 注册Service
+	srv := flux.Service{
+		Kind:      "flux.service/inspect/v1",
+		Interface: ServiceStatsServiceInterface,
+		Method:    ServiceStatsServiceMethod,
+		Attributes: []flux.Attribute{
+			{Name: flux.ServiceAttrTagRpcProto, Value: flux.ProtoInApp},
+		},
+	}
+	ext.RegisterService(srv)
+	inapp.RegisterInvokeFunc(srv.ServiceID(), ServiceStatsInvokeFunc)
+}
+
+// ServiceStatsInvokeFunc 查询Service元数据统计的函数实现
+func ServiceStatsInvokeFunc(_ *flux.Context, _ flux.Service) (interface{}, *flux.ServeError) {
+	protos := make(map[string]int)
+	total := 0
+	for _, ep := range ext.Services() {
+		total++
+		proto := ep.RpcProto()
+		if c, ok := protos[proto]; ok {
+			protos[proto] = c + 1
+		} else {
+			protos[proto] = 1
+		}
+	}
+	return map[string]interface{}{
+		"success": true,
+		"data": map[string]interface{}{
+			"count":  total,
+			"protos": protos,
+		},
+	}, nil
+}
